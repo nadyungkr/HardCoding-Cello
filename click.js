7340a7410e7b89789050efd99f1194564b34cf2d
nadyungkr <dyungkr@naver.com>
2020-03-10 23:15:14 +0900

    onclick 기능 추가(탑메뉴, 푸터) gnb ul > li > ul height 수정

---
+++ click.js
@@ -0,0 +1,17 @@
+function dropDown() {
+    var x = document.getElementById('dropSub');
+    if (x.style.display === 'block') {
+      x.style.display = 'none';
+    } else {
+      x.style.display = 'block';
+    }
+}
+
+function dropTopDown() {
+    var y = document.getElementById('lang');
+    if (y.style.height === '0px') {
+      y.style.height = '160%';
+    } else {
+      y.style.height = '0px';
+    }
+}
